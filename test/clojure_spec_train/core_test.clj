68eadc64998d99bc2736eea251fb2ab4474d1aa3
kero_hirake <k16.h.white@gmail.com>
2021-12-28 15:29:17 +0900

    lein new app

---
+++ test/clojure_spec_train/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-spec-train.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-spec-train.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
